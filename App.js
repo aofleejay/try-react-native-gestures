f7b8b7035199a83e845ebfdc429a64c003088de1
aofleejay <aofleejay@hotmail.com>
2018-02-21 00:13:39 +0700

    add drag me component with gestures

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, PanResponder } from 'react-native'
 
 export default class App extends React.Component {
+  state = {
+    top: 0,
+    left: 0,
+    topTransition: 0,
+    leftTransition: 0,
+  }
+
+  componentWillMount() {
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (e, gestureState) => {
+        return true
+      },
+      onPanResponderMove: (e, gestureState) => {
+        const { dy, dx } = gestureState
+
+        this.setState({
+          topTransition: dy,
+          leftTransition: dx,
+        })
+      },
+      onPanResponderEnd: (e, gestureState) => {
+        const { top, left } = this.state
+        const { dy, dx } = gestureState
+
+        this.setState({
+          top: top + dy,
+          left: left + dx,
+          topTransition: 0,
+          leftTransition: 0,
+        })
+      }
+    })
+  }
+
   render() {
+    const { top, left, topTransition, leftTransition } = this.state
+    const style = {
+      top: top + topTransition,
+      left: left + leftTransition,
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app</Text>
+      <View
+        {...this.panResponder.panHandlers}
+        style={[styles.box, style]}
+      >
+        <Text>DRAG ME</Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  box: {
+    position: 'absolute',
+    backgroundColor: 'salmon',
+    width: 100,
+    height: 100,
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+})
